6db413f3e86f179003c71c47703334f2e33c0785
javier fernandez marugan <jmarugan@iota21.aulas.gsyc.es>
2014-10-23 14:35:43 +0200

    Practica 4 punto 6 terminado

--- client.py
+++ client.py
@@ -15,7 +15,8 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = sys.argv[3:]
+REGISTER= sys.argv[3].upper()
+LINE = sys.argv[4:]
 LINE = " ".join(LINE)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
@@ -24,7 +25,7 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+my_socket.send(REGISTER + ' sip:' + LINE + ' SIP/2.0' + '\r\n''\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
--- server.py
+++ server.py
@@ -15,23 +15,27 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
-        print(self.client_address)
+        self.wfile.write("Hemos recibido tu peticion" + '\r\n')
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
+            #print("envio ------> " + str(line[0]))
             line = self.rfile.read()
-            print(line)
-            if line[0:8] == 'REGISTER':
+            lista= line.split(" ")
 
-                Dic_clients[self.client_address(1)]= self.client_address(2)
+            if lista[0] == 'REGISTER':
+
+                Nick = lista[1].split(":")
+                Dic_clients[Nick[1]]= self.client_address[0]
                 print ("Cliente Registrado")
-     
+                print 'El cliente nos manda: ' + line
+                self.wfile.write("SIP/2.0 200 OK" + '\r\n')
             if not line:
+            
                 break
 
-            print "El cliente nos manda " + line
-
 if __name__ == "__main__":
+
     # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])
     Dic_clients ={}
